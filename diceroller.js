1d5d69663754bc8ade5c36d7fb24a9b6042b548f
Your Name <you@example.com>
2023-01-29 18:28:30 -0600

    Finish assignment

--- diceroller.js
+++ diceroller.js
@@ -1,39 +1,22 @@
-// Try changing the capitalization of the the function name.
-// Try changing "x" to "X" in the difference calculation.
-
+// Variables
+var cashSum = 250;
+var previousRoll = 0;
+// Dice roll function
 function RollSixSidedDice() {
-    document.getElementById("dice").value = Math.floor(Math.random()*6) + 1;
+    var roll1 = document.getElementById("dice").value = Math.floor(Math.random()*6) + 1;
+    var roll2 = document.getElementById("dice2").value = Math.floor(Math.random()*6) + 1;
+    var currentRole = document.getElementById("yourRoll").value = roll1 + roll2;
+    if (currentRole > previousRoll) {
+        previousRoll = currentRole;
+        cashSum = document.getElementById("money").value = cashSum * 2;
+    } else {
+        cashSum = document.getElementById("money").value = cashSum * 0;
+        alert("You lose!");
+        location.reload();
+    }
 }
 
-function DoCalculations() {
-    x = document.getElementById("xID").value;
-    y = document.getElementById("yID").value;
-    console.log("x="+x+" and y="+y);
-
-    x = Number(x);
-    y = Number(y);
-
-    sum = x+y;
-    difference = x-y;
-    product = x*y;
-    quotient = x/y;
-
-    mathResults = "sum="+sum+"<br>";
-    mathResults += "difference="+difference+"<br>";
-    mathResults += "product="+product+"<br>";
-    mathResults += "quotient="+quotient+"<br>";
-    document.getElementById("mathResultsID").innerHTML = mathResults;
-
-    var a = 7;
-    var b = 2;
-    if (a  == b || a > 5) {
-        console.log("123");
-        if (b > 3 && a < 8) {
-            console.log("456");
-        } else {
-            console.log("789");
-        }
-        if (a+b > 9)
-        console.log("10");
-    }
+function cashOut() {
+    alert("You won $" + cashSum + "!");
+    location.reload();
 }
