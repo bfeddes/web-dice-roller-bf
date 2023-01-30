ebb1ec4fdf00678304d8505c8f524bbbbd930ccc
Your Name <you@example.com>
2023-01-29 19:13:19 -0600

    Finished Project

--- diceroller.js
+++ diceroller.js
@@ -1,14 +1,28 @@
+/*
+    Author:  Brian Feddes
+    Class:   Software Engineering 
+    Description: Web Dice Roller
+*/
+
 // Variables
 var cashSum = 250;
 var previousRoll = 0;
+
+// Prints the starting balance
+function printStart() {
+    cashSum = cashSum;
+    document.getElementById("money").value = "$" + cashSum;
+}
+
 // Dice roll function
 function RollSixSidedDice() {
-    var roll1 = document.getElementById("dice").value = Math.floor(Math.random()*6) + 1;
-    var roll2 = document.getElementById("dice2").value = Math.floor(Math.random()*6) + 1;
+    var roll1 = document.getElementById("dice").value = Math.floor(Math.random()*10) + 1;
+    var roll2 = document.getElementById("dice2").value = Math.floor(Math.random()*10) + 1;
     var currentRole = document.getElementById("yourRoll").value = roll1 + roll2;
     if (currentRole > previousRoll) {
         previousRoll = currentRole;
-        cashSum = document.getElementById("money").value = cashSum * 2;
+        cashSum = cashSum * 2;
+        document.getElementById("money").value = "$" + cashSum;
     } else {
         cashSum = document.getElementById("money").value = cashSum * 0;
         alert("You lose!");
@@ -16,6 +30,7 @@ function RollSixSidedDice() {
     }
 }
 
+//Sending cash out message
 function cashOut() {
     alert("You won $" + cashSum + "!");
     location.reload();
